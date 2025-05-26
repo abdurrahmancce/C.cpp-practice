0fda62bbcec7e40be06cdac78cac228fb2f6dcd4
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2025-05-26 04:25:59 -0500

    Update G. Summation from 1 to N.cpp

--- ICPC ASSIUT University Problem Solving Sheet 1/G. Summation from 1 to N.cpp
+++ ICPC ASSIUT University Problem Solving Sheet 1/G. Summation from 1 to N.cpp
@@ -1,3 +1,11 @@
 //G. Summation from 1 to N
 
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    cout << (long long)n * (n + 1) / 2 << endl;
+    return 0;
+}
