2d94dc071ab00a40c8ee189fe76b403c1b436259
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2025-06-05 05:52:14 -0500

    Update C. Simple Calculator.cpp

--- ICPC ASSIUT University Problem Solving Sheet 1/C. Simple Calculator.cpp
+++ ICPC ASSIUT University Problem Solving Sheet 1/C. Simple Calculator.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 int main() {
- long long x,y;
+long long x,y;
 cin >> x >>y;
 cout <<x<<" + "<<y<< " = " <<x+y<<'\n';
 
@@ -12,3 +12,22 @@ cout <<x<<" * "<<y<< " = " <<x*y<<'\n';
 cout <<x<<" - "<<y<< " = " <<x-y<<'\n';
 }
 
+// Another Method
+/*
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a,b,c,d,e;
+    cin>>a>>b;
+    c=a+b;
+    d=a*b;
+    e=a-b;
+    cout<<a<<" + "<<b<<" = "<<c<<endl;
+    cout<<a<<" * "<<b<<" = "<<d<<endl;
+    cout<<a<<" - "<<b<<" = "<<e<<endl;
+    //cout<<a <<" + "<<b<<" = "<<c<<endl<<a<<" * "<<b<<" = "<<d<<endl<<a<<" - "<<b<<" = "<<e<<endl;
+}
+
+*/
