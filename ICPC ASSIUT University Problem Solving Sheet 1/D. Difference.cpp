f5d3a7e0bbbdd34e3e6134096a02850014e1dea0
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2025-06-05 06:07:50 -0500

    Update D. Difference.cpp

--- ICPC ASSIUT University Problem Solving Sheet 1/D. Difference.cpp
+++ ICPC ASSIUT University Problem Solving Sheet 1/D. Difference.cpp
@@ -1,5 +1,17 @@
 //D. Difference
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<"Difference = " <<(a*b)-(c*d);
+}
+
+
+//  Another Method 2
+/*
 #include <iostream>
 using namespace std;
 int main()
@@ -12,4 +24,21 @@ int main()
 
     return 0;
 }
+*/
 
+//  Another Method 3
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a,b,c,d,e,f,g;
+    cin>>a>>b>>c>>d;
+    e=a*b;
+    f=c*d;
+    g=e-f;
+    cout<<"Difference = "<<g;
+    return 0;
+}
+*/
