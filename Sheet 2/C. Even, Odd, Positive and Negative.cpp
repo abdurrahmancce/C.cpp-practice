e995e2c0e1e51ecfdbd6bd6d76e1277942de02cf
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2025-06-11 05:28:36 -0500

    ICPC Assiut University Community Sheet 2(Loops)

--- Sheet 2/C. Even, Odd, Positive and Negative.cpp
+++ Sheet 2/C. Even, Odd, Positive and Negative.cpp
@@ -28,3 +28,44 @@ int main()
     }
     cout<<"Even: "<<e<<endl<<"Odd: "<<o<<endl<<"Positive: "<<p<<endl<<"Negative: "<<n<<endl;
 }
+
+//Another Method
+/*
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long N;
+    cin >> N;
+    long long even = 0, odd = 0, positive = 0, negative = 0;
+    for (int i = 0; i < N; i++)
+    {
+        long long x;
+        cin >> x;
+
+        if (x % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+
+        if (x > 0)
+        {
+            positive++;
+        }
+        else if (x < 0)
+        {
+            negative++;
+        }
+    }
+    cout << "Even: " << even << endl;
+    cout << "Odd: " << odd << endl;
+    cout << "Positive: " << positive << endl;
+    cout << "Negative: " << negative << endl;
+    return 0;
+}
+*/
+
