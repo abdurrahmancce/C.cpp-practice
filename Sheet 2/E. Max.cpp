e811163863e094fcadbd012d3e1db0d57af35c73
Abdur Rahman <e241018@ugrad.iiuc.ac.bd>
2025-06-12 05:43:32 -0500

    ICPC Assiut University Community Sheet 2(Loops)

--- Sheet 2/E. Max.cpp
+++ Sheet 2/E. Max.cpp
@@ -1,6 +1,28 @@
 //E. Max
 
 #include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    int max = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int x;
+        cin >> x;
+        if (x > max)
+        {
+            max = x;
+        }
+    }
+    cout << max << endl;
+    return 0;
+}
+
+//Another Method
+/*
+#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -21,3 +43,4 @@ int main()
 
     return 0;
 }
+*/
